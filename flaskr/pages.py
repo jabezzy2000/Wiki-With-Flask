e3b32d2835830cc2b0600f183f149ad8ebc6053c
Jabez Agyemang-Prempeh <jabez.agyemang-prem@techexchange.in>
2023-03-06 21:40:36 +0000

    Made initial edits. Problem with link to index.html

--- flaskr/pages.py
+++ flaskr/pages.py
@@ -9,6 +9,12 @@ def make_endpoints(app):
     def home():
         # TODO(Checkpoint Requirement 2 of 3): Change this to use render_template
         # to render main.html on the home page.
-        return render_template("main.html")
+        page_links = [    {"name": "Home", "url": "/"},    {"name": "Pages", "url": "/index"},    {"name": "About", "url": "/about"},    {"name": "Login", "url": "/login"},    {"name": "Sign up", "url": "/signup"}]
+        greeting = "Welcome to our Wiki page! We hope you love it here."        
+        return render_template("main.html", greeting= greeting, page_links = page_links)
 
     # TODO(Project 1): Implement additional routes according to the project requirements.
+    @app.route('/index')
+    def index():
+        pages = ['dbz', 'tekken']
+        return render_template('index.html', pages = pages)
--- run-flask.sh
+++ run-flask.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 export FLASK_APP=flaskr
 export FLASK_ENV=development
 flask run -p 8080
